6cb706258162ff4e9b8ba2d657fb2dd10f601634
theoandres <77053383+theoandres@users.noreply.github.com>
2025-02-25 05:32:53 +0100

    Update to_do_v3_oop.py

--- My_first_projects/to_do_v3_oop.py
+++ My_first_projects/to_do_v3_oop.py
@@ -1,4 +1,6 @@
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
+from pathlib import Path
+
 
 class Task:
     def __init__(self, name, description, priority):
@@ -49,9 +51,13 @@ class TaskManager:
 
 
     def show_tasks(self):
-        for idx, task in enumerate(self.task_memory, start=1):
-            status = "completed" if task.completed else "not completed"
-            print(f"{idx} : {task.name} - {task.description}, Priority: {task.priority}, Status: {status}")
+        if len(self.task_memory) >= 1:
+            for idx, task in enumerate(self.task_memory, start=1):
+                status = "completed" if task.completed else "not completed"
+                print(f"{idx} : {task.name} - {task.description}, Priority: {task.priority}, Status: {status}")
+        else:
+            print('U have no tasks to do.')
+
 
     def save_and_exit(self):
         wb = Workbook()
@@ -60,8 +66,26 @@ class TaskManager:
         ws.append(['Name', 'Description', 'Priority', 'Completed'])
         for task in self.task_memory:
             ws.append([task.name, task.description, task.priority, task.completed])
-        wb.save('todolist_save.xlsx')
+        wb.save('todolistsave.xlsx')
         print('Tasks successfully saved')
+
+
+    def remove_task(self):
+        self.show_tasks()
+        if len(self.task_memory) > 0:
+            while True:
+                try:
+                    choice = int(input('Enter a number of task to delete: '))
+                    if 1 <= choice <= len(self.task_memory):
+                        self.task_memory.pop(choice - 1)
+                        print('Task was successfully deleted.')
+                        break
+                    else:
+                        print('Invalid input, please try again.')
+                except ValueError:
+                    print('Invalid input, please enter a valid value')
+        else:
+             print('No tasks to delete')
 
 
     def main_menu(self):
@@ -71,7 +95,8 @@ Choose of the following:
 1 - Add task
 2 - Show tasks
 3 - Mark task as completed
-4 - Save and exit
+4 - Remove task
+5 - Save and exit
  """)
             choice = input()
             if choice == '1':
@@ -81,6 +106,8 @@ Choose of the following:
             elif choice == '3':
                 self.complete_task()
             elif choice == '4':
+                self.remove_task()
+            elif choice == '5':
                 self.save_and_exit()
                 break
             else:
@@ -89,4 +116,19 @@ Choose of the following:
 
 if __name__ == "__main__":
     task_manager = TaskManager()
+    file = Path('todolistsave.xlsx')
+    if file.exists():
+        wb = load_workbook(filename='todolistsave.xlsx')
+        ws = wb.active
+        for row in ws.iter_rows(min_row = 2, values_only = True):
+            task = Task(name = row[0], description = row[1], priority = row[2])
+            task_manager.task_memory.append(task)
+    else:
+        task_manager.task_memory = []
+        print('Task manager is empty')
+
     task_manager.main_menu()
+
+
+
+
